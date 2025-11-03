6e369b3c7f92f7a066f651f45b95a579bdb5a33a
Hyeri Jang <chjang5621@naver.com>
2025-11-03 21:27:34 +0900

    feat : LocalDateTime 직렬화/역직렬화 기능 추가

--- src/main/kotlin/com/langdy/langdy_backend_assignment/dto/LessonRequest.kt
+++ src/main/kotlin/com/langdy/langdy_backend_assignment/dto/LessonRequest.kt
@@ -1,15 +1,14 @@
 package com.langdy.langdy_backend_assignment.dto
 
-import jakarta.validation.constraints.NotNull
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
 
+import java.time.LocalDateTime
+
+@Serializable
 data class LessonRequest(
-    @field:NotNull
-    val startAt: String?,
-
-    @field:NotNull
-    val courseId: Long?,
-
-    @field:NotNull
-    val teacherId: Long?
+    @Contextual
+    val startAt: LocalDateTime,
+    val courseId: Long,
+    val teacherId: Long
 )
-
